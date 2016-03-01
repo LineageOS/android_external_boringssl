96c7333fb36ff5d46633bba4c19f7af912fe3fc4
David Benjamin <davidben@google.com>
2016-03-01 17:35:47 -0500

    Rewrite ARM feature detection.
    This removes the thread-unsafe SIGILL-based detection and the
    multi-consumer-hostile CRYPTO_set_NEON_capable API. (Changing
    OPENSSL_armcap_P after initialization is likely to cause problems.)
    
    The right way to detect ARM features on Linux is getauxval. On aarch64,
    we should be able to rely on this, so use it straight. Split this out
    into its own file. The #ifdefs in the old cpu-arm.c meant it shared all
    but no code with its arm counterpart anyway.
    
    Unfortunately, various versions of Android have different missing APIs,
    so, on
    arm, we need a series of workarounds. Previously, we used a SIGILL
    fallback
    based on OpenSSL's logic, but this is inherently not thread-safe.
    (SIGILL also
    does not tell us if the OS knows how to save and restore NEON state.)
    Instead,
    base the behavior on Android NDK's cpu-features library, what Chromium
    currently uses with CRYPTO_set_NEON_capable:
    
    - Android before API level 20 does not provide getauxval. Where missing,
    we can read from /proc/self/auxv.
    
    - On some versions of Android, /proc/self/auxv is also not readable, so
    use /proc/cpuinfo's Features line.
    
    - Linux only advertises optional features in /proc/cpuinfo. ARMv8 makes
    NEON
    mandatory, so /proc/cpuinfo can't be used without additional effort.
    
    Finally, we must blacklist a particular chip because the NEON unit is
    broken
    (https://crbug.com/341598).
    
    Unfortunately, this means CRYPTO_library_init now depends on /proc being
    available, which will require some care with Chromium's sandbox. The
    simplest solution is to just call CRYPTO_library_init before entering
    the sandbox.
    
    It's worth noting that Chromium's current EnsureOpenSSLInit function
    already
    depends on /proc/cpuinfo to detect the broken CPU, by way of base::CPU.
    android_getCpuFeatures also interally depends on it. We were already
    relying on
    both of those being stateful and primed prior to entering the sandbox.
    
    BUG=chromium:589200
    
    javelinanddart: Update sources.mk accordingly for this
    change to compile and work properly as it was intended
    
    Change-Id: Ic5d1c341aab5a614eb129d8aa5ada2809edd6af8
    Reviewed-on: https://boringssl-review.googlesource.com/7506
    Reviewed-by: David Benjamin <davidben@google.com>
    Backported-by: David Daynard <nardholio@gmail.com>

--- sources.mk
+++ sources.mk
@@ -106,7 +106,9 @@ crypto_sources := \
   src/crypto/cipher/tls_cbc.c\
   src/crypto/cmac/cmac.c\
   src/crypto/conf/conf.c\
+  src/crypto/cpu-aarch64-linux.c\
   src/crypto/cpu-arm.c\
+  src/crypto/cpu-arm-linux.c\
   src/crypto/cpu-intel.c\
   src/crypto/crypto.c\
   src/crypto/curve25519/curve25519.c\
@@ -348,7 +350,6 @@ linux_arm_sources := \
   linux-arm/crypto/sha/sha256-armv4.S\
   linux-arm/crypto/sha/sha512-armv4.S\
   src/crypto/chacha/chacha_vec_arm.S\
-  src/crypto/cpu-arm-asm.S\
   src/crypto/curve25519/asm/x25519-asm-arm.S\
   src/crypto/poly1305/poly1305_arm_asm.S\
 
