a332e79aea2b4a08ffc90b3bb22c4403253bce29
David Benjamin <davidben@google.com>
2017-03-20 17:16:19 -0400

    Fix potential memory leak in ASN1_TIME_to_generalizedtime()
    If ret is allocated, it may be leaked on error.
    
    (Imported from upstream's cdfb7809b6a365a0a7874afd8f8778c5c572f267 and
    ffcdb0e6efb6fb7033b2cd29e8cca2e2fe355c14.)
    
    Change-Id: I50ed9ad072cf80461d9527d0834b596a8c32e3d3
    Reviewed-on: https://boringssl-review.googlesource.com/14315
    Reviewed-by: Steven Valdez <svaldez@google.com>
    Commit-Queue: Steven Valdez <svaldez@google.com>
    CQ-Verified: CQ bot account: commit-bot@chromium.org <commit-bot@chromium.org>

--- src/crypto/asn1/a_time.c
+++ src/crypto/asn1/a_time.c
@@ -130,7 +130,7 @@ int ASN1_TIME_check(ASN1_TIME *t)
 /* Convert an ASN1_TIME structure to GeneralizedTime */
 ASN1_GENERALIZEDTIME *ASN1_TIME_to_generalizedtime(ASN1_TIME *t, ASN1_GENERALIZEDTIME **out)
 	{
-	ASN1_GENERALIZEDTIME *ret;
+	ASN1_GENERALIZEDTIME *ret = NULL;
 	char *str;
 	int newlen;
 
@@ -139,22 +139,24 @@ ASN1_GENERALIZEDTIME *ASN1_TIME_to_generalizedtime(ASN1_TIME *t, ASN1_GENERALIZE
 	if (!out || !*out)
 		{
 		if (!(ret = ASN1_GENERALIZEDTIME_new ()))
-			return NULL;
-		if (out) *out = ret;
+			goto err;
 		}
-	else ret = *out;
+	        else
+		{
+			ret = *out;
+	        }
 
 	/* If already GeneralizedTime just copy across */
 	if (t->type == V_ASN1_GENERALIZEDTIME)
 		{
 		if(!ASN1_STRING_set(ret, t->data, t->length))
-			return NULL;
-		return ret;
+			goto err;
+		goto done;
 		}
 
 	/* grow the string */
 	if (!ASN1_STRING_set(ret, NULL, t->length + 2))
-		return NULL;
+		goto err;
 	/* ASN1_STRING_set() allocated 'len + 1' bytes. */
 	newlen = t->length + 2 + 1;
 	str = (char *)ret->data;
@@ -164,8 +166,17 @@ ASN1_GENERALIZEDTIME *ASN1_TIME_to_generalizedtime(ASN1_TIME *t, ASN1_GENERALIZE
 
 	BUF_strlcat(str, (char *)t->data, newlen);
 
+done:
+	if (out != NULL && *out == NULL)
+		*out = ret;
 	return ret;
+
+err:
+	if (out == NULL || *out != ret)
+		ASN1_GENERALIZEDTIME_free(ret);
+	return NULL;
 	}
+
 
 int ASN1_TIME_set_string(ASN1_TIME *s, const char *str)
 	{
